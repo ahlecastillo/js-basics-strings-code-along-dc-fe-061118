501e91c568ed99e7541bac739fc25ea5dbb03670
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-23 00:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUppercase()
+const excitedWelcomeMessage = welcomeMessage.toUppercase();
+
