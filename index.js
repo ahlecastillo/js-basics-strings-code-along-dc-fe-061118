231306792d6728a00868ab0d5997caf60d0cc845
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-23 00:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0)}!`;
 
-const ShortGreeting = 
